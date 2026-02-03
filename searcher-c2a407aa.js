a5a417cf3d03a53a349a2927b6d78593aa45d52c
kalbis09 <kalbis09@users.noreply.github.com>
2026-02-03 06:34:09 +0000

    deploy: 3fae83b5ef3925e387002863d70d5dbc182020ed

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-ed44a391.js',
+                path_to_root + 'searchindex-f0863e59.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
